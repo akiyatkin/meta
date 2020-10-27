735d643d1509f1ce671b18c5604dab21e0ec8b51
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-10-27 10:43:21 +0400

    fix

--- Meta.php
+++ Meta.php
@@ -17,8 +17,33 @@ class MetaException extends \Exception {
 class Meta {	
 	use CacheOnce; //once($name, $args, $fn) , $once
 	public $list = [];
+
 	public function __construct($name = 'meta', $lang = 'ru', $src = false, $base = false) {
 		
+	}
+	public function init($opt = []) {
+		extract(array_merge([
+			'handlers'=> [],
+			'name' => 'meta',
+			'base' => false,
+			'lang' => 'ru',
+			'action' => explode('/', str_replace($opt['base'] ?? '/-'.$opt['name'], '', explode('?',$_SERVER['REQUEST_URI'],2)[0]), 3)[1] ?? ''
+		], $opt), EXTR_REFS);
+
+
+		$this->action = $action;
+		$this->ans = [];//'action'=> $this->action
+		$this->lang = $lang;
+		$this->name = $name;
+		
+		try {
+			foreach ($handlers as $hand) $this->get($hand);
+			$this->get($this->action);
+			//$this->_ret('meta.ready');
+		} catch (MetaException $e) {
+			return $this->ans;
+		}
+
 	}
 	public function &add($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$from = null;
@@ -48,12 +73,14 @@ class Meta {
 			}
 		}
 		$this->list[$pname] = [
+			'name' => $pname,
 			'process' => false,
 			'request' => false, //Нужно ли брать из REQUEST
 			'required' => false, //Нужно ли выкидывать исключение если нет request
 			'result' => null,
 			'ready' => false,
 			'cache' => false,
+			'type' => null,
 			'func' => $func,
 			'after' => $after,
 			'before' => $before,			
@@ -63,62 +90,50 @@ class Meta {
 	}
 	public function addHandler($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$opt = &$this->add($pname, $a1, $a2, $a3);
+		$opt['type'] = 'handler';
 		$opt['cache'] = true;
 		$opt['request'] = false;
 		$opt['required'] = false;
 	}
 	public function addArgument($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$opt = &$this->add($pname, $a1, $a2, $a3);
+		$opt['type'] = 'argument';
 		$opt['cache'] = true;
 		$opt['request'] = true;
 		$opt['required'] = true;
 	}
 	public function addVariable($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$opt = &$this->add($pname, $a1, $a2, $a3);
-		
+		$opt['type'] = 'variable';
 		$opt['cache'] = true;
 		$opt['request'] = false;
 		$opt['required'] = false;
 	}
 	public function addAction($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$opt = &$this->add($pname, $a1, $a2, $a3);
+		$opt['type'] = 'action';
 		$opt['cache'] = true;
 		$opt['request'] = false;
 		$opt['required'] = false;
 	}
 	public function addFunction($pname, $a1 = null, $a2 = null, $a3 = null) {
 		$opt = &$this->add($pname, $a1, $a2, $a3);
+		$opt['type'] = 'function';
 		$opt['cache'] = false;
 		$opt['request'] = false;
 		$opt['required'] = false;
 	}
+	public function empty() {	
+		$actions = implode(', ', array_keys(array_filter($this->list, function ($h) {
+			if (!$h['name']) return false;
+			return $h['type'] == 'action';
+		})));
+	 	return $this->_err('meta.emptyrequest', $actions);
+	}
 	public function is($pname) {
 		return $this->list[$pname]['ready'];
 	}
-	public function init($opt = []) {
-		extract(array_merge([
-			'handlers'=> [],
-			'name' => 'meta',
-			'base' => false,
-			'lang' => 'ru',
-			'action' => explode('/', str_replace($opt['base'] ?? '/-'.$opt['name'], '', explode('?',$_SERVER['REQUEST_URI'],2)[0]), 3)[1] ?? ''
-		], $opt), EXTR_REFS);
-
-
-		$this->action = $action;
-		$this->ans = ['action'=> $this->action];
-		$this->lang = $lang;
-		$this->name = $name;
-		
-		try {
-			foreach ($handlers as $hand) $this->get($hand);
-			$this->get($this->action);
-			//$this->_ret('meta.ready');
-		} catch (MetaException $e) {
-			return $this->ans;
-		}
-
-	}
+	
 	public function gets($pnames) {
 		foreach ($pnames as $pname) {
 			$vname = preg_split('/[@\?]/', $pname)[0];
@@ -168,7 +183,7 @@ class Meta {
 	}
 
 
-	public function addBacktraceLines($count = 8) {
+	public function addBacktraceLines($count = 3) {
 		$back = debug_backtrace();
 		array_splice($back, sizeof($back) - 5);
 		foreach ($back as $i => $e) {
@@ -206,7 +221,7 @@ class Meta {
 		$ans['backtrace'] = $lines;
 	}
 
-	public function _fail($namecode, $pname = false) {
+	public function _fail($namecode, $pname = null) {
 		$ans = &$this->ans;
 		$lang = $this->list['lang']['result'] ?? $this->lang;
 
@@ -220,7 +235,7 @@ class Meta {
 		
 		
 
-		if (!$pname) {
+		if (is_null($pname)) {
 			Lang::fail($ans, $lang, $namecode.'.'.$this->action.'-'.$this->addBacktraceLines());
 			throw new MetaException();
 		}
@@ -228,10 +243,12 @@ class Meta {
 		Lang::failtpl($ans, $lang, $namecode);
 		throw new MetaException();
 	}
-	public function fail($code, $pname = false) {
+	public function fail($code = null, $pname = null) {
+		if (!$code) return Lang::err($this->ans);
 		return $this->_fail($this->name.'.'.$code, $pname);
 	}
-	public function _err($namecode, $pname = false) {
+
+	public function _err($namecode, $pname = null) {
 		$ans = &$this->ans;
 		$lang = $this->list['lang']['result'] ?? $this->lang;
 		if (Access::isDebug()) {
@@ -241,7 +258,7 @@ class Meta {
 			}));
 		}
 
-		if (!$pname) {
+		if (is_null($pname)) {
 			$ans = Lang::err($ans, $lang, $namecode);
 			throw new MetaException();
 		} 
@@ -249,10 +266,12 @@ class Meta {
 		$ans = Lang::errtpl($ans, $lang, $namecode);
 		throw new MetaException();
 	}
-	public function err($code, $pname = false) {
+	public function err($code = null, $pname = null) {
+		if (!$code) return Lang::err($this->ans);
 		return $this->_err($this->name.'.'.$code, $pname);
 	}
-	public function _ret($namecode = false, $pname = false) {
+
+	public function _ret($namecode = false, $pname = null) {
 		$ans = &$this->ans;
 		$lang = $this->list['lang']['result'] ?? $this->lang;
 		if (Access::isDebug()) {
@@ -262,7 +281,7 @@ class Meta {
 			}));
 		}
 
-		if (!$pname) {
+		if (is_null($pname)) {
 			Lang::ret($ans, $lang, $namecode);
 			throw new MetaException();
 		}
@@ -271,8 +290,8 @@ class Meta {
 		Lang::rettpl($ans, $lang, $namecode);
 		throw new MetaException();
 	}
-	public function ret($code, $pname = false) {
-		if (!$code) return Lang::ret($ans);
+	public function ret($code = null, $pname = null) {
+		if (!$code) return Lang::ret($this->ans);
 		return $this->_ret($this->name.'.'.$code, $pname);
 	}
 
